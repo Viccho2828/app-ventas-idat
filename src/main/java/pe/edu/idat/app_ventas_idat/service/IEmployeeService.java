05b57b726a3d3309cde48fcb1e8c1e69e756b3dd
Viccho2828 <kikekollizion@gmail.com>
2024-06-27 20:53:26 -0500

    Controller

--- src/main/java/pe/edu/idat/app_ventas_idat/service/IEmployeeService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/IEmployeeService.java
@@ -1,4 +1,12 @@
 package pe.edu.idat.app_ventas_idat.service;
 
+import pe.edu.idat.app_ventas_idat.model.bd.Category;
+
+import java.util.List;
+
 public interface IEmployeeService {
+
+    List<Category> listarempleados();
+
+    Category guardarEmpleado(Category category);
 }
